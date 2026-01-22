29558760fdb2cc70d61c51838b24b3eda758bf19
ipwan <elitex2006@yandex.ru>
2026-01-22 17:41:59 +0300

    test second sensor

--- distance_sensors/test_distance_sensors.py
+++ distance_sensors/test_distance_sensors.py
@@ -3,11 +3,10 @@ from gpiozero import DistanceSensor
 
 
 print("Prepare GPIO Pins")
-# sensor_r = DistanceSensor(echo=5, trigger=6, queue_len=2)
 sensor_l = DistanceSensor(echo=17, trigger=27, queue_len=2)
+sensor_r = DistanceSensor(echo=5, trigger=6, queue_len=2)
 
 while True:
-    # print(f"left: {sensor_l.distance * 100}, "
-    #       f"Right: {sensor_r.distance * 100}")
-    print(f"left: {sensor_l.distance * 100}")
+    print(f"left: {sensor_l.distance * 100}, "
+          f"Right: {sensor_r.distance * 100}")
     sleep(0.1)
