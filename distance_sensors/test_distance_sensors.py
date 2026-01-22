0322dc91c47fa731d0dcfae6a79767314e010a71
ipwan <elitex2006@yandex.ru>
2026-01-22 14:27:52 +0300

    add sensor HC-SR04

---
+++ distance_sensors/test_distance_sensors.py
@@ -0,0 +1,12 @@
+from time import sleep
+from gpiozero import DistanceSensor
+
+
+print("Prepare GPIO Pins")
+sensor_l = DistanceSensor(echo=5, trigger=6, queue_len=2)
+sensor_r = DistanceSensor(echo=17, trigger=27, queue_len=2)
+
+while True:
+    print(f"left: {sensor_l.distance * 100}, "
+          f"Right: {sensor_r.distance * 100}")
+    sleep(0.1)
