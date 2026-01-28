e783482520f8416d812bed77fd172ce2a4990923
ipwan <elitex2006@yandex.ru>
2026-01-28 15:16:14 +0300

    Обход препятствий

---
+++ distance_sensors/robot.py
@@ -0,0 +1,44 @@
+from Raspi_MotorHAT import Raspi_MotorHAT
+from gpiozero import DistanceSensor
+import atexit
+
+
+class Robot:
+
+    def __init__(self, motorhat_addr=0x6f):
+        # Setup the motorhat with the passed in address
+        self._mh = Raspi_MotorHAT(addr=motorhat_addr)
+        # получение локальных переменных для каждого двигателя
+        self.left_motor = self._mh.getMotor(1)
+        self.right_motor = self._mh.getMotor(2)
+        # настройка датчиков расстояния
+        self.left_distance_sensor = DistanceSensor(echo=17, trigger=27,
+                                                   queue_len=2)
+        self.right_distance_sensor = DistanceSensor(echo=5, trigger=6,
+                                                    queue_len=2)
+        atexit.register(self.stop_all)
+
+        def convert_speed(self, speed):
+            # Выбор режима работы
+            mode = Raspi_MotorHAT.RELEASE
+            if speed > 0:
+                mode = Raspi_MotorHAT.FORWARD
+            elif speed < 0:
+                mode = Raspi_MotorHAT.BACKWARD
+            # Определение частоты вращения
+            output_speed = (abs(speed) * 255) // 100
+            return mode, int(output_speed)
+
+        def set_left(self, speed):
+            mode, output_speed = self.convert_speed(speed)
+            self.left_motor.setSpeed(output_speed)
+            self.left_motor.run(mode)
+
+        def set_right(self, speed):
+            mode, output_speed = self.convert_speed(speed)
+            self.right_motor.setSpeed(output_speed)
+            self.right_motor.run(mode)
+
+        def stop_all(self):
+            self.left_motor.run(Raspi_MotorHAT.RELEASE)
+            self.right_motor.run(Raspi_MotorHAT.RELEASE)
---
+++ distance_sensors/simple_avoid_behavior.py
@@ -0,0 +1,36 @@
+from robot import Robot
+from time import sleep
+
+
+class ObstacleAvoidingBehavior:
+    """Простой обход препятствий"""
+    def __init__(self, the_robot):
+        self.robot = the_robot
+        self.speed = 60
+
+    def get_motor_speed(self, distance):
+        """Этот метод вычисляет частоту вращения двигателя на основе
+        данных о расстоянии, полученных от датчика"""
+        if distance < 0.2:
+            return -self.speed
+        return self.speed
+
+    def run(self):
+        self.robot.set_pan(0)
+        self.robot.set_titl(0)
+        while True:
+        # Получение показаний от датчика в метрах
+            left_distance = self.robot.left_distance_sensor.distance
+            right_distance = self.robot.right_distance_sensor.distance
+            print(f"Left: {left_distance:.2f}, Right: {right_distance:.2f}")
+            left_speed = self.get_motor_speed(left_distance)
+            self.robot.set_left(left_speed)
+            right_speed = self.robot.right_distance_sensor.distance
+            self.robot.set_right(right_speed)
+            sleep(0.05)
+
+
+
+bot = Robot()
+behavior = ObstacleAvoidingBehavior(bot)
+behavior.run()
