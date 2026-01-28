763fb6cd31d6e95315e0de130a8e16382529cf13
ipwan <elitex2006@yandex.ru>
2026-01-28 15:26:14 +0300

    fix Robot class

--- distance_sensors/robot.py
+++ distance_sensors/robot.py
@@ -18,27 +18,27 @@ class Robot:
                                                     queue_len=2)
         atexit.register(self.stop_all)
 
-        def convert_speed(self, speed):
-            # Выбор режима работы
-            mode = Raspi_MotorHAT.RELEASE
-            if speed > 0:
-                mode = Raspi_MotorHAT.FORWARD
-            elif speed < 0:
-                mode = Raspi_MotorHAT.BACKWARD
-            # Определение частоты вращения
-            output_speed = (abs(speed) * 255) // 100
-            return mode, int(output_speed)
+    def convert_speed(self, speed):
+        # Выбор режима работы
+        mode = Raspi_MotorHAT.RELEASE
+        if speed > 0:
+            mode = Raspi_MotorHAT.FORWARD
+        elif speed < 0:
+            mode = Raspi_MotorHAT.BACKWARD
+        # Определение частоты вращения
+        output_speed = (abs(speed) * 255) // 100
+        return mode, int(output_speed)
 
-        def set_left(self, speed):
-            mode, output_speed = self.convert_speed(speed)
-            self.left_motor.setSpeed(output_speed)
-            self.left_motor.run(mode)
+    def set_left(self, speed):
+        mode, output_speed = self.convert_speed(speed)
+        self.left_motor.setSpeed(output_speed)
+        self.left_motor.run(mode)
 
-        def set_right(self, speed):
-            mode, output_speed = self.convert_speed(speed)
-            self.right_motor.setSpeed(output_speed)
-            self.right_motor.run(mode)
+    def set_right(self, speed):
+        mode, output_speed = self.convert_speed(speed)
+        self.right_motor.setSpeed(output_speed)
+        self.right_motor.run(mode)
 
-        def stop_all(self):
-            self.left_motor.run(Raspi_MotorHAT.RELEASE)
-            self.right_motor.run(Raspi_MotorHAT.RELEASE)
+    def stop_all(self):
+        self.left_motor.run(Raspi_MotorHAT.RELEASE)
+        self.right_motor.run(Raspi_MotorHAT.RELEASE)
