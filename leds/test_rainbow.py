f46fd480c857108033887127de496e8b3c12faf5
ipwan <elitex2006@yandex.ru>
2026-01-29 21:26:00 +0300

    fix led count

--- leds/test_rainbow.py
+++ leds/test_rainbow.py
@@ -6,7 +6,7 @@ from led_rainbow import show_rainbow
 bot = Robot()
 while True:
     print("on")
-    show_rainbow(bot.leds, range(bot.leds.led_count))
+    show_rainbow(bot.leds, range(bot.leds.count))
     bot.leds.show()
     sleep(0.5)
     print("of")
