05bce67d9bc3d336808abfe007404f8bf0a2e8fb
ipwan <elitex2006@yandex.ru>
2026-01-29 21:22:40 +0300

    add rainbow

---
+++ leds/test_rainbow.py
@@ -0,0 +1,15 @@
+from robot import Robot
+from time import sleep
+from led_rainbow import show_rainbow
+
+
+bot = Robot()
+while True:
+    print("on")
+    show_rainbow(bot.leds, range(bot.led_count))
+    bot.leds.show()
+    sleep(0.5)
+    print("of")
+    bot.leds.clear()
+    bot.leds.show()
+    sleep(0.5)
